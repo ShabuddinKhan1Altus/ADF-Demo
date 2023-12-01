2e67185383ce83f192f1faeba23417dee94f29ea
ShabuddinKhan1Altus <ShabuddinKhan1Altus>
2023-12-01 15:32:06 +0000

    update

--- install-falcon.sh
+++ install-falcon.sh
@@ -1,9 +1,9 @@
 mkdir /tmp/crowdstrike
 cd /tmp/crowdstrike
-curl -k 'https://stappnpeunotf01.blob.core.windows.net/crowdstrike/falcon-sensor-7.05.0-16004.el8.x86_64.rpm?sp=r&st=2023-12-01T11:20:54Z&se=2023-12-01T19:20:54Z&spr=https&sv=2022-11-02&sr=b&sig=3qFmoHx0%2BYzrFwdls8zIztNtb4QEmS4ruEjoGe0MDC0%3D' --output 'falcon-sensor-7.05.0-16004.el8.x86_64.rpm'
-ls -ltr
-dpkg --help
-#sudo dpkg -i falcon-sensor-7.05.0-16004.el8.x86_64.rpm
-#sudo /opt/CrowdStrike/falconctl -s --cid=86B2879505B640F583CBC4D47D5E82F1-88
-#service falcon-sensor start
-#systemctl start falcon-sensor
+curl -k 'https://stappnpeunotf01.blob.core.windows.net/crowdstrike/falcon-sensor_7.05.0-16004_amd64.deb?sp=r&st=2023-12-01T15:30:12Z&se=2023-12-01T23:30:12Z&sip=20.166.128.74&spr=https&sv=2022-11-02&sr=b&sig=GBOGxFoKv2Myu%2BrjWFjZ9evx9mTYlpsh6oYVBbiE2QY%3D' --output 'falcon-sensor_7.05.0-16004_amd64.deb'
+# ls -ltr
+# dpkg --help
+sudo dpkg -i falcon-sensor_7.05.0-16004_amd64.deb
+sudo /opt/CrowdStrike/falconctl -s --cid=86B2879505B640F583CBC4D47D5E82F1-88
+service falcon-sensor start
+systemctl start falcon-sensor
